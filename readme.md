17fec1efa8f1e61dc78cedde9679501fc4c857f7
Gabor Pankotay <mrpanky@gmail.com>
2020-04-01 13:21:57 +0200

    simple google auth poc impl.

---
+++ readme.md
@@ -0,0 +1,5 @@
+keystore jelszó: stayathome
+
+reverse_client_id: 348200934618-9080un5sk68i29s1gfu1v7g4us9nn79c.apps.googleusercontent.com
+
+ionic cordova plugin add cordova-plugin-googleplus --save --variable REVERSED_CLIENT_ID=348200934618-9080un5sk68i29s1gfu1v7g4us9nn79c.apps.googleusercontent.com
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import * as firebase from 'firebase/app';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -9,9 +10,17 @@ import { TabsPage } from '../pages/tabs/tabs';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBxLqlT6KgQWSSxYlXijZcHKwcjJj76XSE',
+      authDomain: 'stay-at-home-ionic.firebaseapp.com',
+      databaseURL: 'https://stay-at-home-ionic.firebaseio.com',
+      projectId: 'stay-at-home-ionic',
+      storageBucket: 'stay-at-home-ionic.appspot.com',
+      messagingSenderId: '159351352235'
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
