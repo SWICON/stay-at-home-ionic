9930847fd3e40080198b1d71fdd696f85cbc1d3f
Zsendej Tihamer <zstihamer@gmail.com>
2020-04-03 16:55:19 +0200

    home screan mock

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,58 +1,58 @@
-import { Component } from '@angular/core';
-// import * as firebase from 'firebase';
-// import { GooglePlus } from '@ionic-native/google-plus/ngx';
-import { Platform, ToastController } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import {Platform, ToastController} from '@ionic/angular';
+import {AuthenticationService} from '../shared/authentication-service';
+import {User} from '../shared/user';
+import {UserSettings} from '../shared/user-settings.interface';
+import {UserSettingsService} from '../shared/user-settings.service';
+
+const oneDay = 1000 * 60 * 60 * 24;
+
+function daysBetween(one, another) {
+    return Math.round(Math.abs((+one) - (+another)) / oneDay);
+}
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-  isUserLoggedIn: any = false;
-  userData: any = {};
+export class Tab1Page implements OnInit {
+    isUserLoggedIn: any = false;
+    userSettings: UserSettings;
+    user: User;
+    daysInIsolation: number;
 
-  constructor(private platform: Platform,
-              private toaster: ToastController,
-              // private googleplus: GooglePlus
-  ) {
-    // firebase.auth().onAuthStateChanged(authData => {
-    //   if (authData != null) {
-    //     this.isUserLoggedIn = true;
-    //     this.userData = authData;
-    //     console.log(authData);
-    //   } else {
-    //     this.userData = {};
-    //   }
-    // });
-  }
+    points = {
+        collected: 15,
+        reward: 18,
+        penalty: 3
+    };
 
-  // logout() {
-  //   firebase.auth().signOut();
-  //   this.isUserLoggedIn = false;
-  // }
+    localArea: any = {
+        km10: 1534,
+        km20: 23023,
+        km100: 523420
+    };
 
-  displayToast(message) {
-    this.toaster.create({ message, duration: 3000 });
-  }
+    constructor(private platform: Platform,
+                private toaster: ToastController,
+                private userService: UserSettingsService,
+                private auth: AuthenticationService) {
 
-  // doGoogleLogin() {
-  //   // browser login
-  //   if (this.platform.is('desktop')) {
-  //     firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(gpRes => {
-  //       this.displayToast('Login Success');
-  //       this.userData = gpRes.additionalUserInfo.profile;
-  //     }).catch(err => this.displayToast(err));
-  //   } else {
-  //     this.googleplus.login({
-  //       webClientId: '...'
-  //     }).then((success) => {
-  //       console.log(success);
-  //       const credential = firebase.auth.GoogleAuthProvider.credential(success.idToken, null);
-  //       firebase.auth().signInWithCredential(credential).then((data) => {
-  //         console.log(data);
-  //       }).catch((err) => this.displayToast(err));
-  //     }, err => this.displayToast(err));
-  //   }
-  // }
+        this.platform.ready().then(async () => {
+            this.user = await this.auth.getUser();
+            this.userSettings = await this.userService.getUserSettings(this.user)
+                .then((res: UserSettings) => {
+                    this.daysInIsolation = daysBetween(new Date(res.isolationStartedAt), new Date());
+                    return res;
+                });
+        });
+
+    }
+
+
+    ngOnInit(): void {
+    }
+
+
 }
